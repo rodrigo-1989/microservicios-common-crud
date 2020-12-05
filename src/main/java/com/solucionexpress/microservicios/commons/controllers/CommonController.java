b58caec8949ab904a6c519027d4e832aa4a9b809
Julio <rodrigo_l321@hotmail.com>
2020-12-05 17:35:27 -0600

    Validacion de atributos

--- src/main/java/com/solucionexpress/microservicios/commons/controllers/CommonController.java
+++ src/main/java/com/solucionexpress/microservicios/commons/controllers/CommonController.java
@@ -4,10 +4,14 @@ package com.solucionexpress.microservicios.commons.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import com.solucionexpress.microservicios.commons.services.CommonService;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 
@@ -31,17 +35,25 @@ public class CommonController<E,S extends CommonService<E>> {
     }
 
     @PostMapping
-    public ResponseEntity<?> crear(@RequestBody E entity){
+    public ResponseEntity<?> crear(@Valid @RequestBody E entity, BindingResult result){
+        if (result.hasErrors() ){
+            return this.validar(result);
+        }
         E entityDb = service.save( entity );
-
         return ResponseEntity.status(HttpStatus.CREATED).body( entityDb );
     }
-
-
 
     @DeleteMapping("/{id}")
     public ResponseEntity <?> eliminar(@PathVariable Long id ){
          service.deleteById(id);
          return ResponseEntity.noContent().build();
+    }
+
+    protected ResponseEntity<?>validar(BindingResult result){
+        Map<String, Object> errores =new  HashMap<>();
+        result.getFieldErrors().forEach(err -> {
+            errores.put(err.getField(),"El campo "+ err.getField()+" "+ err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errores);
     }
 }
